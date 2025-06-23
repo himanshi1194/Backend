67237b465b3a7435519bc990a09735617e634542
Jay Thakor <jaysinhdarbar730@gmail.com>
2025-06-23 22:18:36 +0530

    Implemented Doc fetch Api

--- index.js
+++ index.js
@@ -2,30 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const path = require("path");
 
-const authRoutes = require("./routes/authRoutes")
-const docRoutes = require("./routes/docRoutes")
+const authRoutes = require("./routes/authRoutes");
+const docRoutes = require("./routes/docRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
-//Routes  
-app.use("/api/auth", authRoutes)
-app.get("/", (req,res) =>{
-  res.send("Api is running")
-})
-app.use("/api/docs", docRoutes)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+app.use("/api/docs", docRoutes);
 
 // Connect DB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB error", err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error", err));
 
 // Test route
 app.get("/", (req, res) => res.send("API Running"));
--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const upload = require("../middleware/upload");
 const auth = require("../middleware/authMiddleware");
 const uploadDocument = require("../controllers/docController");
+const document = require("../models/Document")
 
 router.post("/upload", auth, upload.single("pdf"), uploadDocument);
+router.get("/", auth, async (req,res)=>{
+    try {
+        const docs = await document.find({user:req.user}).sort({uploadedAt: -1})
+        res.json(docs)
+    } catch (error) {
+        res.status(500).json({ msg:"Server error"})
+    }
+})
+
 
 module.exports = router;
